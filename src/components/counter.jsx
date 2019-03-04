0db6e633cf75d27d5a45da9280c96b003f64a9fd
Sumit Arora <sumit1234@gmail.com>
2019-03-04 11:40:14 +1000

    added a simple method to include dynamic style classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,13 +4,20 @@ class Counter extends Component {
   state = {
     count: 0
   };
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary"> {this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
